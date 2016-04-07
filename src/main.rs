7529c6c1570f907c1f1aa1dccd42e29efa65fd3e
Ian McLoughlin <mcloughlin.ian@gmail.com>
2016-04-07 18:31:50 +0100

    Need to research algorithm.

--- src/main.rs
+++ src/main.rs
@@ -18,26 +18,47 @@ fn print(x:u64, n:usize) {
   println!("{x:0>0$b}", n as usize, x = x >> 1);
 }
 
-fn genneck(b:&mut u64, n:usize, k:usize, t:usize, p:usize) {
-  if t > n {
-    if n % p == 0 {
-      print(*b, n);
-    }
-  } else {
-    let x = get(*b, t - p);
-    set(b, t, x);
-    genneck(b, n, k, t + 1, p);
-    if x == 0 {
-      set(b, t, 1);
-      genneck(b, n, k, t + 1, t);
+struct Bracelets {
+  b:u64
+  , n:usize
+  , k:usize
+  , t:usize
+  , p:usize
+}
+
+impl Iterator for Bracelets {
+  type Item = u64;
+
+  fn next(&mut self) -> Option<u64> {
+
+    if self.t > self.n {
+      if self.n % self.p == 0 {
+        Some(self.b);
+      }
+    } else {
+      let x = get(self.b, self.t - self.p);
+      set(self.b, self.t, self.x);
+      //???genneck(b, n, k, t + 1, p);
+      if x == 0 {
+        set(self.b, self.t, 1);
+        //??genneck(self.b, self.n, self.k, self.t + 1, self.t);
+      }
     }
   }
 }
 
-fn main() {
-  let mut b:u64 = 0;
-  let n:usize = 16;
-  let k:usize = 2;
-
-  genneck(&mut b, n, k, 1, 1);
+fn bracelets(n:usize, k:usize) -> Bracelets {
+  Bracelets {b:0, n:n, k:k, t:1, p:1}
 }
+
+
+fn main() {
+  for i in bracelets() {
+    print(i, n);
+  }
+}
+
+
+
+
+
